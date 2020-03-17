71668a65ee4d9207f466dac68430cf46a4f506c1
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2020-03-17 01:06:28 -0400

    update index.js

--- models/index.js
+++ models/index.js
@@ -3,17 +3,20 @@
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
-var basename = path.basename(__filename);
+var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.js")[env];
 var db = {};
 
-let sequelize
-if (env === "development") {
-   sequelize = new Sequelize(config);
+
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
- sequelize = new Sequelize(
-    process.env.JAWSDB_URL);
+  var sequelize = new Sequelize(
+    config.database, 
+    config.username, 
+    config.password, 
+    config);
 }
 
 fs
